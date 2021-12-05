eb392fa3b82be89b6cca7959c04a0fe1ce7d84ca
ThaiVu54 <thai01654369440@gmail.com>
2021-12-05 11:51:22 +0700

    restful

---
+++ src/main/java/com/thai/cms/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.thai.cms.service;
+
+import com.thai.cms.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+
+}
---
+++ src/main/java/com/thai/cms/service/IGeneralService.java
@@ -0,0 +1,15 @@
+package com.thai.cms.service;
+
+import com.thai.cms.model.Customer;
+
+import java.util.Optional;
+
+public interface IGeneralService<T> {
+    Iterable<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    T save(T t);
+
+    void remove(Long id);
+}
